9b795cabda6d3693bc4c0012d28b59e885cb2a59
Moatasem-Elsayed <eng.moatasem.8@gmail.com>
2022-05-31 13:47:48 +0200

    [#Session 1] -explain basics of Makefile

---
+++ s1_basics/analysis.py
@@ -0,0 +1,5 @@
+
+print("Code speed is : 65 % ")
+print("Code size is : 70% ")
+print("Code testing is : Pass ")
+print("Code Readability is : Pass ")
---
+++ s1_basics/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello from cpp" << std::endl;
+    return 0;
+}
